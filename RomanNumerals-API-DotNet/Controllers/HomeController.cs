0832afbdb07b678dd246e1af9f6bbb7ce1854b8c
Razede Myrrh Guibone <razede.guibone@netsells.co.uk>
2022-04-25 11:13:59 +0800

    Feature: Update DotNet Core version to 3.1 Changes: Updated runtime framework, launch settings, app settings, program, startup and project dependencies. Added sample logger dependency and privacy page.
    NSI-13

--- RomanNumerals-API-DotNet/Controllers/HomeController.cs
+++ RomanNumerals-API-DotNet/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using RomanNumerals_API_DotNet.Models;
+using System.Diagnostics;
 
 namespace RomanNumerals_API_DotNet.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
